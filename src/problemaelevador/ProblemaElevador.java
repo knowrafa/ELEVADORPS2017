ef62bc21e43e81c15699ee307dd87e49c496adb6
knowrafa <noprafa@gmail.com>
2017-10-01 11:13:59 -0300

    Revert "Este commit é um teste"
    This reverts commit c469d20208cb0a1646f9fe7ac5cdc53d532ae889.

--- src/problemaelevador/ProblemaElevador.java
+++ src/problemaelevador/ProblemaElevador.java
@@ -40,6 +40,8 @@ public class ProblemaElevador {
         //edificio.setMusicInElevator(4, "Young Folks");
         //edificio.setMusicInElevator(5, "El Fuego");
         
+        //FUNCIONAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+        
         for(int i=0; i < 2; i++){
             String temporary = JOptionPane.showInputDialog("Qual o Piso Atual?");
             if(temporary.equals("")){
